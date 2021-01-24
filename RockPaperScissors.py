7447f7b14f8ef9c516a20fb3d891b91398e18209
Nick Sackey <nicksackey@hotmail.com>
2021-01-24 22:46:01 +0000

    Added some error checking to RockPaperScissors

--- RockPaperScissors.py
+++ RockPaperScissors.py
@@ -6,28 +6,32 @@
 player1 = input("Player 1 make your selection between rock, paper or scissors: ")
 player2 = input("Player 2 now make your selection ")
 
-if player1 == "rock":
-	if player2 == "scissors":
-		print("Player 1 wins!")
-	elif player2 == "paper":
-		print("Player 2 wins!")
-	else:
-		print("It's a tie!")
+if player1 == "rock" or "paper" OR "scissors" and player2 == "rock" or "paper" OR "scissors":
+	if player1 == "rock":
+		if player2 == "scissors":
+			print("Player 1 wins!")
+		elif player2 == "paper":
+			print("Player 2 wins!")
+		else:
+			print("It's a tie!")
 
-if player1 == "paper":
-	if player2 == "rock":
-		print("Player 1 wins!")
-	elif player2 == "scissors":
-		print("Player 2 wins!")
-	else:
-		print("It's a tie!")
+	if player1 == "paper":
+		if player2 == "rock":
+			print("Player 1 wins!")
+		elif player2 == "scissors":
+			print("Player 2 wins!")
+		else:
+			print("It's a tie!")
 
-if player1 == "scissors":
-	if player2 == "paper":
-		print("Player 1 wins!")
-	elif player2 == "rock":
-		print("Player 2 wins!")
-	else:
-		print("It's a tie!")
+	if player1 == "scissors":
+		if player2 == "paper":
+			print("Player 1 wins!")
+		elif player2 == "rock":
+			print("Player 2 wins!")
+		else:
+			print("It's a tie!")
+else:
+	print("You have to type in rock, paper or scissors")
+
 
 
