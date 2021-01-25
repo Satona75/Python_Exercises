5b5cb2266d38afcae1678ac79a22a1c49559b5a6
Nick Sackey <nicksackey@hotmail.com>
2021-01-25 21:42:06 +0000

    Added a more advanced Rock, Paper, Scissors game where opponent is the computer

---
+++ RPS-AI.py
@@ -0,0 +1,42 @@
+#This game plays Rock, Paper, Scissors against the computer
+print("Rock...")
+print("Paper...")
+print("Scissors...\n")
+
+#Player is invited to choose first
+player=input("Make your move: ")
+
+#Number is randomly generated between 0 and 2
+import random
+comp_int=random.randint(0, 2)
+
+if comp_int == 0:
+    computer = "rock"
+elif comp_int == 1:
+    computer = "paper"
+elif comp_int == 2:
+    computer = "scissors"
+
+print("Computer has chosen: " + computer)
+
+if computer == player:
+    print("It's a tie!")
+elif computer == "rock":
+    if player == "paper":
+        print("You Win!")
+    elif player == "scissors":
+        print("Computer Wins!")
+elif computer == "paper":
+    if player == "scissors":
+        print("You Win!")
+    elif player == "rock":
+        print("Computer Wins!")
+elif computer == "scissors":
+    if player == "rock":
+        print("You Win!")
+    elif player == "paper":
+        print("Computer Wins!")
+else:
+    pint("Something has gone wrong!")
+        
+
