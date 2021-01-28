32fb84e173fe4d5149c35584a8509be67edc1d07
Nick Sackey <nicksackey@hotmail.com>
2021-01-28 20:32:01 +0000

    Repeater Exercise

---
+++ Repeater.py
@@ -0,0 +1,8 @@
+#User inputs a number and the Statement "Clean your Room!" is printed out that many times
+
+repeat = input("How many times do I have to tell you? " )
+
+repeat = int(repeat)
+for repeater in range(0,repeat):
+    print("CLEAN UP YOUR ROOM!")
+    
