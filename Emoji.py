ae6cca3f8c865865a09b48acac2573ed91f4c082
Nick Sackey <nicksackey@hotmail.com>
2021-01-29 21:48:39 +0000

    Addition of emoji exercises

---
+++ Emoji.py
@@ -0,0 +1,23 @@
+# For loop String multiplication version
+#for num in range (1,11):
+#    print("\U0001f600" * num)
+
+# While loop string multiplication version
+#num = 1
+#while num <= 10:
+#    print("\U0001f600" * num)
+#    num += 1
+
+#Combination of for and while loop in place of string multiplication
+#for num in range(1,11):
+    # count = 1
+    # smileys = ""
+    # while count <= num:
+    #     smileys +="\U0001f600"
+    #     count += 1
+    # print(smileys)
+
+#Creating a centered triangle of emojis
+for num in range (1,21,2):
+    print((" " *(20 - num)) + ("\U0001f600" * num))
+
