d6d1c97c410f6e4d311f24a6dbe8654fd14c5667
Nick Sackey <nicksackey@hotmail.com>
2021-01-29 22:14:38 +0000

    CopyCat exercise

---
+++ CopyCat.py
@@ -0,0 +1,9 @@
+# This program takes what ever the user says and copies it until they say "stop copying me"
+
+copyme = input("Hey how's it going? ")
+
+while copyme !="stop copying me":
+    print(copyme)
+    copyme = input()
+print("Alight! I'll Stop!")
+
