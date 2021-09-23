702daec3f9541692c8770e12f5f89acf19a92e5b
Nick Sackey <Nick.Sackey@Civica.co.uk>
2021-09-23 21:14:28 +0100

    Additional Projects

--- Coffee_Machine/main.py
+++ Coffee_Machine/main.py
@@ -1,10 +1,8 @@
 # TODO Import Coffee data
 # Import menu data from the data file
-
 from Data import MENU, resources
 
 # Declare a dictionary variable that will contain the resources remaining after each drink order
-
 machine_resources = {}
 
 machine_resources["water"] = resources["water"]
@@ -12,20 +10,21 @@ machine_resources["milk"] = resources["milk"]
 machine_resources["coffee"] = resources["coffee"]
 machine_resources["money"] = 0
 
-
-# Function for extracting the resource values
-def resource_availability(water, milk, coffee):
-    """Determines if there are enough resources to make the drink. Returns true if there
-    are and False if they are not"""
-    if water < machine_resources["water"] and milk < machine_resources["milk"] and coffee < machine_resources["coffee"]:
-        return True
-    else:
-        return False
+#Function for checking resource availability
+def resource_availability(drink_resources):
+    """Checks there is enough resources to make the drink. Returns True is there is and False if there isn't"""
+    for item in drink_resources:
+        if drink_resources[item] > machine_resources[item]:
+            print(f"Sorry there is not enough {item}")
+            return False
+        else:
+            return True
 
 # Function for calculating what resources are left after the drink is made
 def resource_balance():
     """Subtracts the resources used by the drink from the total and returns the values left"""
-    return machine_resources["water"] - drink_water, machine_resources["milk"] - drink_milk, \
+    return machine_resources["water"] - drink_water, \
+           machine_resources["milk"] - drink_milk, \
            machine_resources["coffee"] - drink_coffee
 
 
@@ -33,18 +32,21 @@ def resource_balance():
 def total_currency(quarter, dime, nickle, penny):
     return (quarter * 0.25) + (dime * 0.1) + (nickle * 0.05) + (penny * 0.01)
 
-# Function to determine how much chang is required
+# Function to determine how much change is required
 def change_given(amount_paid, price):
+    """Returns how much change is due"""
     return amount_paid - price
 
+#Main Coffee Maker Program
+is_on = True
 
-# TODO Print What would you like? (espresso/latte/cappuccino)
-
-while True:
+while is_on:
     coffee_choice = input("What would you like? (espresso/latte/cappuccino)").lower()
 
-    # TODO To report on the resources (water, milk, coffee, money) available
-    # Display report if report is entered in as the choice
+# Display report if report is entered in as the choice
+
+    if coffee_choice == "off":
+        is_on = False
 
     if coffee_choice == "report":
         print(f"Water: {machine_resources['water']}ml")
@@ -53,7 +55,9 @@ while True:
         print(f"Money: ${machine_resources['money']}")
 
     else:
-    # Resource details for the chosen drink are examined
+# Resource details for the chosen drink are examined
+        drink = MENU[coffee_choice]
+        # drink = MENU[coffee_choice]["ingredients"]
 
         drink_water = MENU[coffee_choice]["ingredients"]["water"]
 
@@ -66,11 +70,10 @@ while True:
 
         drink_coffee = MENU[coffee_choice]["ingredients"]["coffee"]
 
-    # TODO Ensure there are enough resources to make the drink
     # Determine if the drink choice can be made
 
-        if resource_availability(drink_water, drink_milk, drink_coffee):
-            # TODO Ask the customer to insert coins (quarters, dimes, nickles and pennies)
+        if resource_availability(drink["ingredients"]):
+
             print("Please insert coins")
             quarters = int(input("How many Quarters?"))
             dimes = int(input("How many Dimes?"))
@@ -81,25 +84,22 @@ while True:
 
             print(f"Total Payment is ${total_payment}")
 
-    # TODO Ensure the customer has inserted enough money and provide change if required
-            if total_payment >= MENU[coffee_choice]["cost"]:
+
+            if total_payment >= drink["cost"]:
                 print(f"Here is your {coffee_choice}. Enjoy!")
-                change = change_given(total_payment, MENU[coffee_choice]["cost"])
+                change = change_given(total_payment, drink["cost"])
                 print(f"Here is ${change:.2f} in change")
-                machine_resources["money"] += MENU[coffee_choice]["cost"]
+                machine_resources["money"] += drink["cost"]
 
 
-    # TODO Whatever is ordered the ingredients are subtracted from the available resources
-    # Update the balance of the resources
+
+                # Update the balance of the resources
                 machine_resources["water"], machine_resources["milk"], machine_resources["coffee"] = resource_balance()
-                print(f"Water balance is {machine_resources['water']}")
-                print(f"Milk balance is {machine_resources['milk']}")
-                print(f"Water balance is {machine_resources['coffee']}")
+
 
             else:
                 print("Sorry that's not enough money. Money refunded")
-        else:
-                print("Sorry there is not enough water.")
+
 
 
 
