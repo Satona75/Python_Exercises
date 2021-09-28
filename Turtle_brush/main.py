582b037c58ab129f0865aa61ffe4a2c40b5293cc
Nick Sackey <nicksackey@hotmail.com>
2021-09-28 21:38:44 +0100

    Updated Turtle Module

--- Turtle_brush/main.py
+++ Turtle_brush/main.py
@@ -1,10 +1,23 @@
-import another_module
+#import another_module
 
-print(another_module.another_variable)
+#print(another_module.another_variable)
 
-from turtle import Turtle
+from turtle import Turtle, Screen
 
 timmy = Turtle()
 print(timmy)
+timmy.shape("turtle")
+timmy.color("red")
+
+timmy.circle(100)
+timmy.forward(100)
+
+my_screen = Screen()
+#print(my_screen.canvheight)
+my_screen.exitonclick()
+
+
+
+
 
 
