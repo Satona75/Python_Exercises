8674df9f2e7242095f19e0c7d6e2efb32623a214
Nick Sackey <nicksackey@hotmail.com>
2021-10-02 22:38:57 +0100

    File Updates

--- Turtle_brush/main.py
+++ Turtle_brush/main.py
@@ -2,20 +2,28 @@
 
 #print(another_module.another_variable)
 
-from turtle import Turtle, Screen
+# from turtle import Turtle, Screen
+#
+# timmy = Turtle()
+# print(timmy)
+# timmy.shape("turtle")
+# timmy.color("red")
+#
+# timmy.circle(100)
+# timmy.forward(100)
+#
+# my_screen = Screen()
+# #print(my_screen.canvheight)
+# my_screen.exitonclick()
 
-timmy = Turtle()
-print(timmy)
-timmy.shape("turtle")
-timmy.color("red")
+from prettytable import PrettyTable
 
-timmy.circle(100)
-timmy.forward(100)
+table = PrettyTable()
 
-my_screen = Screen()
-#print(my_screen.canvheight)
-my_screen.exitonclick()
-
+table.add_column('Pokemon Name',['Pikachu', 'Squirtle','Charmander'])
+table.add_column('Type',['Electric','Water', 'Fire'])
+table.align = "l"
+print(table)
 
 
 
